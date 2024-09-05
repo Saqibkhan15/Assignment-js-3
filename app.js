27aaa17687687b9dc6da39ac55a602cda10e61cd
Saqibkhan15 <aqib12u@gmail.com>
2024-09-06 01:48:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// Q1
+// var city = prompt("Enter your city:");
+// if (city === "karachi") {
+//     alert("Welcome to the city of lights");
+// }
+// Q2
+// var gender = prompt("Gender:");
+// if (gender === "male") {
+//     alert("Good Morning Sir");
+// } else if(gender === "female") {
+//     alert("Good Morning Ma’am");}
